816bd116bbe2ab73cec8cf7bd14a29ab1d16d508
Enzo <enzo77_7@hotmail.com>
2026-04-20 18:17:31 -0300

    20_04

--- pruebas.py
+++ pruebas.py
@@ -9,7 +9,7 @@
 #print(len(num_list))
 #print(num_list)
 
-num_list =[1,3,5,7,10,5,1,1,15]
+#num_list =[1,3,5,7,10,5,1,1,15]
 
 #num_list.append(7)
 #print(num_list)
@@ -24,3 +24,29 @@ num_list =[1,3,5,7,10,5,1,1,15]
 #x = num_list.pop(4)
 #print(num_list)
 #print (x)
+
+# Funciones y Modulo
+
+#var_global = 45
+
+#def mi_funcion(num1, num2, otro = 'hola mundo', nuevo='otro valor'):
+ #   var_global = 123
+  #  print(otro)
+   # print(nuevo)
+    #print(var_global)
+    #return num1 + num2, num1 - num2
+
+#num1 = 5
+#num2 = 3
+#lista_nums = [1, 2, 3, 4]   
+
+#suma, diferencia = mi_funcion(num1, num2, nuevo='Pepe')
+#print (suma,diferencia)
+#print(var_global)
+
+#import mi_modulo
+
+#resultado = mi_modulo.suma(10,5)
+#print (resultado)
+
+#print(mi_modulo.suma(19,9))
